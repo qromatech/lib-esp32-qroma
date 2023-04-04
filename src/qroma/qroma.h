a6f5c9ce603116667d2260bbf15caaa156174a10
Albert Boehmler <ajboehmler@gmail.com>
2023-04-04 10:24:18 -0400

    Updating for importability

--- src/qroma/qroma.h
+++ src/qroma/qroma.h
@@ -2,13 +2,16 @@
 #define QROMA_H
 
 
-#include "startup/qroma_startup.h"
 #include "qroma-app/QromaAppConfig.h"
+
 #include "qroma-comm/commBuffer/QromaCommMemBuffer.h"
 #include "qroma-comm/pb-commands/PbCommandHandler.h"
 #include "qroma-comm/pb-commands/PbCommandWithResponseHandler.h"
 #include "qroma-comm/qroma-comm.h"
-#include "startup/register/registerPbCommandHandlers.h"
 
+#include "startup/qroma_startup.h"
+#include "startup/register/registerPbCommandHandlers.h"
+#include "startup/configure/configureQromaApp.h"
+#include "startup/configure/configureQromaSerialCommApp.h"
 
 #endif
---
+++ src/qroma_lib.h
@@ -0,0 +1,8 @@
+#ifndef QROMA_LIB_H
+#define QROMA_LIB_H
+
+
+#include "qroma/qroma.h"
+
+
+#endif
